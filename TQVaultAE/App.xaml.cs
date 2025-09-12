4e63af7af6f825d2d6e6706513ae2225145fb377
unknown <Leopold.Pohlmann@gmx.net>
2025-09-12 23:38:09 +0200

    Add .NET 8 WPF project for new UI

---
+++ TQVaultAE/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TQVaultAE
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			try
+			{
+				base.OnStartup(e);
+				new MainWindow().Show();
+			} 
+			catch (Exception ex)
+			{
+				// TODO Handle general exceptions
+			}
+		}
+	}
+
+}
