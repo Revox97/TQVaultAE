9c466ea8b873fec2f489434c510c89f93fd22a30
unknown <Leopold.Pohlmann@gmx.net>
2025-09-13 03:58:23 +0200

    Add pages and removed additional columndefinition

---
+++ src/TQVaultAE/Pages/SettingsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace TQVaultAE.Pages
+{
+	/// <summary>
+	/// Interaction logic for SettingsPage.xaml
+	/// </summary>
+	public partial class SettingsPage : Page
+	{
+		// TODO ADD:
+		// All existing settings
+		// Vault settings
+		// Structure settings in a more clear way
+		public SettingsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
