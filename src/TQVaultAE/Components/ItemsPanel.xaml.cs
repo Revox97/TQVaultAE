c30cf8ab4582ae0a6b7fa18f2d1497af1f792ff1
unknown <Leopold.Pohlmann@gmx.net>
2025-09-14 00:10:58 +0200

    Add itemspanel

---
+++ src/TQVaultAE/Components/ItemsPanel.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TQVaultAE.Components
+{
+    /// <summary>
+    /// Interaction logic for ItemsPanel.xaml
+    /// </summary>
+    public partial class ItemsPanel : UserControl
+    {
+		private readonly int _width;
+		private readonly int _height;
+
+        public ItemsPanel(int width, int height)
+        {
+            InitializeComponent();
+
+			_width = width;
+			_height = height;
+			InitializePanel();
+
+			// Set Items
+        }
+
+		private void InitializePanel()
+		{
+			try
+			{
+				for (int i = 0; i < _width; ++i)
+					ItemsPanelContent.RowDefinitions.Add(new RowDefinition());
+
+				for (int i = 0; i< _height; ++i)
+					ItemsPanelContent.ColumnDefinitions.Add(new ColumnDefinition());
+
+				InitializeCells();
+			}
+			catch (Exception ex)
+			{
+				// TODO handle exception
+			}
+		}
+
+		private void InitializeCells()
+		{
+			if (FindResource("ItemSlot") is Style style)
+			{
+				// TODO optimize this chunk of code
+				for (int r = 0; r < ItemsPanelContent.RowDefinitions.Count; ++r)
+				{
+					for (int c = 0; c < ItemsPanelContent.ColumnDefinitions.Count; ++c)
+					{
+						Border border = new() { Style = style };
+
+						ItemsPanelContent.Children.Add(border);
+						Grid.SetRow(border, r);
+						Grid.SetColumn(border, c);
+					}
+				}
+			}
+			else
+			{
+				throw new ResourceReferenceKeyNotFoundException("Failed to load style for item slot.", "ItemSlot");
+			}
+		}
+    }
+}
--- src/TQVaultAE/Components/Vault.xaml.cs
+++ src/TQVaultAE/Components/Vault.xaml.cs
@@ -10,6 +10,16 @@ namespace TQVaultAE.Components
         public Vault()
         {
             InitializeComponent();
+			CreateItemsPanel();
+        }
+
+		private void CreateItemsPanel()
+		{
+			ItemsPanel panel = new(12, 20);
+
+			Container.Children.Add(panel);
+			Grid.SetRow(panel, 1);
+			Grid.SetColumn(panel, 1);
         }
     }
 }
