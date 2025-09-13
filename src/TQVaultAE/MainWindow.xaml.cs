d680177829457b188a84e51d655242ac4d56f8f3
unknown <Leopold.Pohlmann@gmx.net>
2025-09-13 04:15:57 +0200

    Add toggle button support

--- src/TQVaultAE/MainWindow.xaml.cs
+++ src/TQVaultAE/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 using TQVaultAE.Models;
 
 namespace TQVaultAE
@@ -11,13 +12,49 @@ namespace TQVaultAE
 		public MainWindow()
 		{
 			InitializeComponent();
-
 			DataContext = new MainWindowModel();
 		}
 
 		private void ButtonExit_Click(object sender, RoutedEventArgs e)
 		{
 			Application.Current.Shutdown();
+		}
+
+		private void ButtonVault_Checked(object sender, RoutedEventArgs e)
+		{
+			if (sender is ToggleButton buttonVault && (buttonVault.IsChecked ?? false))
+			{
+				if (ButtonConfiguration is not null)
+					ButtonConfiguration.IsChecked = false;
+
+				if (ButtonSearch is not null)
+					ButtonSearch.IsChecked = false;
+			}
+		}
+
+		private void ButtonConfiguration_Checked(object sender, RoutedEventArgs e)
+		{
+			if (sender is ToggleButton buttonConfiguration && (buttonConfiguration.IsChecked ?? false))
+			{
+				if (ButtonVault is not null)
+					ButtonVault.IsChecked = false;
+
+				if (ButtonConfiguration is not null)
+				ButtonSearch.IsChecked = false;
+			}
+		}
+
+		private void ButtonSearch_Checked(object sender, RoutedEventArgs e)
+		{
+			if (sender is ToggleButton buttonSearch && (buttonSearch.IsChecked ?? false))
+			{
+				if (ButtonVault is not null)
+					ButtonVault.IsChecked = false;
+
+				if (ButtonConfiguration is not null)
+					ButtonConfiguration.IsChecked = false;
+			}
+
 		}
 	}
 }
