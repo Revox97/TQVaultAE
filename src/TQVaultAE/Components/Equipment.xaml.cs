f855dff39cdee80a9d80886ee4ab0153769fac3d
unknown <Leopold.Pohlmann@gmx.net>
2025-09-20 13:25:08 +0200

    Add calculation of equipment grid

--- src/TQVaultAE/Components/Equipment.xaml.cs
+++ src/TQVaultAE/Components/Equipment.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
 using TQVaultAE.Controllers.Observable;
 using TQVaultAE.Models.EventArgs;
+using TQVaultAE.Services;
 
 namespace TQVaultAE.Components
 {
@@ -21,15 +25,153 @@ namespace TQVaultAE.Components
 			SetGridDimensions(args);
 		}
 
-		private void SetGridDimensions(ContentScaleUpdatedEventArgs args)
+		private void SetGridDimensions(ContentScaleUpdatedEventArgs? args = null)
 		{
-			// TODO Set correct column definitions, so items will be inserted in correct positions
-		}
+            if (EquipmentOverlay.Source is null)
+                return;
+
+            byte[] pixels = BitmapPixelSevice.GetRenderedPixels(EquipmentOverlay, out int stride, out int width, out int height);
+            (int, int) columnPositions = SetColumnDefinitions(pixels, stride, height);
+            SetRowDefinitions(pixels, stride, height, columnPositions.Item1, columnPositions.Item2);
+        }
+
+        private (int, int) SetColumnDefinitions(byte[] pixels, int stride, int height)
+        {
+            int completeWidth = 0;
+            int widthColumnZero = GetWidthByPixels(pixels, stride, completeWidth, height, true);
+            completeWidth += widthColumnZero;
+            int widthColumnOne = GetWidthByPixels(pixels, stride, completeWidth, height, false);
+            completeWidth += widthColumnOne;
+            int widthColumnTwo = GetWidthByPixels(pixels, stride, completeWidth, height, true);
+            completeWidth += widthColumnTwo;
+            int widthColumnThree = GetWidthByPixels(pixels, stride, completeWidth, height, false);
+            completeWidth += widthColumnThree;
+            int widthColumnFour = GetWidthByPixels(pixels, stride, completeWidth, height,  true);
+            completeWidth += widthColumnFour;
+            int widthColumnFive = GetWidthByPixels(pixels, stride, completeWidth, height, false);
+            completeWidth += widthColumnFive;
+            int widthColumnSix = GetWidthByPixels(pixels, stride, completeWidth, height, true);
+
+            ItemsContainer.ColumnDefinitions[0].Width = new GridLength(widthColumnZero);
+            ItemsContainer.ColumnDefinitions[1].Width = new GridLength(widthColumnOne);
+            ItemsContainer.ColumnDefinitions[2].Width = new GridLength(widthColumnTwo);
+            ItemsContainer.ColumnDefinitions[3].Width = new GridLength(widthColumnThree);
+            ItemsContainer.ColumnDefinitions[4].Width = new GridLength(widthColumnFour);
+            ItemsContainer.ColumnDefinitions[5].Width = new GridLength(widthColumnFive);
+            ItemsContainer.ColumnDefinitions[6].Width = new GridLength(widthColumnSix);
+
+            int columnLeft = widthColumnZero + 5;
+            int columnMiddle = widthColumnZero + widthColumnOne + widthColumnTwo + 5;
+
+            return (columnLeft, columnMiddle);
+        }
+
+        private static int GetWidthByPixels(byte[] pixels, int stride, int startX, int imageHeight, bool isTransparent)
+        {
+            int y = imageHeight / 2;
+
+            for (int x = startX; x < stride / 4; x++) // divide stride by 4 to get width in pixels
+            {
+                int index = (y * stride) + (x * 4); // proper (x,y) to byte array
+
+                if (index + 3 >= pixels.Length)
+                    break;
+
+                byte alpha = pixels[index + 3];
+
+                if (isTransparent && alpha == 0)
+                    return x - startX;
+
+                if (!isTransparent && alpha != 0)
+                    return x - startX;
+            }
+
+            return (stride / 4) - startX; // default fallback
+        }
+
+        private void SetRowDefinitions(byte[] pixels, int stride, int height, int leftColumn, int middleColumn)
+        {
+            int completeHeight = 0;
+            int heightRowZero = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+            completeHeight += heightRowZero;
+            int heightRowOne = GetHeightByPixels(pixels, stride, completeHeight, leftColumn, height, true);
+            completeHeight += heightRowOne;
+            int heightRowTwo = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, false);
+            completeHeight += heightRowTwo;
+            int heightRowThree = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+            completeHeight += heightRowThree;
+            int heightRowFour = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height,  false);
+            completeHeight += heightRowFour;
+            int heightRowFive = GetHeightByPixels(pixels, stride, completeHeight, leftColumn, height, false);
+            completeHeight += heightRowFive;
+            int heightRowSix = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+            completeHeight += heightRowSix;
+            int heightRowSeven = GetHeightByPixels(pixels, stride, completeHeight, leftColumn, height, true);
+            completeHeight += heightRowSeven;
+            int heightRowEight = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, false);
+            completeHeight += heightRowEight;
+            int heightRowNine = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+            completeHeight += heightRowNine;
+            int heightRowTen = GetHeightByPixels(pixels, stride, completeHeight, leftColumn, height, true);
+            completeHeight += heightRowTen;
+            int heightRowEleven = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, false);
+            completeHeight += heightRowEleven;
+            int heightRowTwelve = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+            completeHeight += heightRowTwelve;
+            int heightRowThirteen = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, false);
+            completeHeight += heightRowThirteen;
+            int heightRowFourteen = GetHeightByPixels(pixels, stride, completeHeight, leftColumn, height, false);
+            completeHeight += heightRowFourteen;
+            int heightRowFifteen = GetHeightByPixels(pixels, stride, completeHeight, middleColumn, height, true);
+
+            ItemsContainer.RowDefinitions[0].Height = new GridLength(heightRowZero);
+            ItemsContainer.RowDefinitions[1].Height = new GridLength(heightRowOne);
+            ItemsContainer.RowDefinitions[2].Height = new GridLength(heightRowTwo);
+            ItemsContainer.RowDefinitions[3].Height = new GridLength(heightRowThree);
+            ItemsContainer.RowDefinitions[4].Height = new GridLength(heightRowFour);
+            ItemsContainer.RowDefinitions[5].Height = new GridLength(heightRowFive);
+            ItemsContainer.RowDefinitions[6].Height = new GridLength(heightRowSix);
+            ItemsContainer.RowDefinitions[7].Height = new GridLength(heightRowSeven);
+            ItemsContainer.RowDefinitions[8].Height = new GridLength(heightRowEight);
+            ItemsContainer.RowDefinitions[9].Height = new GridLength(heightRowNine);
+            ItemsContainer.RowDefinitions[10].Height = new GridLength(heightRowTen);
+            ItemsContainer.RowDefinitions[11].Height = new GridLength(heightRowEleven);
+            ItemsContainer.RowDefinitions[12].Height = new GridLength(heightRowTwelve);
+            ItemsContainer.RowDefinitions[13].Height = new GridLength(heightRowThirteen);
+            ItemsContainer.RowDefinitions[14].Height = new GridLength(heightRowFourteen);
+            ItemsContainer.RowDefinitions[15].Height = new GridLength(heightRowFifteen);
+        }
+
+        private static int GetHeightByPixels(byte[] pixels, int stride, int startY, int startX, int imageHeight, bool isTransparent)
+        {
+            for (int y = startY; y < imageHeight; ++y)
+            {
+                int index = (y * stride) + (startX * 4); // proper (x,y) to byte array
+
+                if (index + 3 >= pixels.Length)
+                    break;
+
+                byte alpha = pixels[index + 3];
+
+                if (isTransparent && alpha == 0)
+                    return y - startY;
+
+                if (!isTransparent && alpha != 0)
+                    return y - startY;
+            }
+
+            return imageHeight - startY; // default fallback
+        }
 
 		public void Dispose()
 		{
 			ContentScaleController.GetInstance().RemoveObserver(this);
 			GC.SuppressFinalize(this);
 		}
-	}
+
+        private void EquipmentBackground_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetGridDimensions();
+        }
+    }
 }
---
+++ src/TQVaultAE/Services/BitmapPixelSevice.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TQVaultAE.Services
+{
+    public static class BitmapPixelSevice
+    {
+
+        internal static byte[] GetRenderedPixels(Image imageControl, out int stride, out int pixelWidth, out int pixelHeight)
+        {
+            if (imageControl.Source is not BitmapSource source)
+                throw new InvalidOperationException("Image source is not a BitmapSource");
+
+            double targetWidth = imageControl.ActualWidth;
+            double targetHeight = imageControl.ActualHeight;
+
+            if (targetWidth <= 0 || targetHeight <= 0)
+                throw new InvalidOperationException("Invalid control size");
+
+            // Source image pixel size
+            double sourceWidth = source.PixelWidth;
+            double sourceHeight = source.PixelHeight;
+
+            // Determine how WPF stretches the image
+            Rect destRect = CalculateImageStretchRect(imageControl.Stretch, sourceWidth, sourceHeight, targetWidth, targetHeight);
+
+            int rtbWidth = (int)Math.Ceiling(targetWidth);
+            int rtbHeight = (int)Math.Ceiling(targetHeight);
+
+            // TODO make sure this is dpi aware
+            RenderTargetBitmap rtb = new(rtbWidth, rtbHeight, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual dv = new();
+            using DrawingContext dc = dv.RenderOpen();
+            
+            // Fill transparent background (optional)
+            dc.DrawRectangle(Brushes.Transparent, null, new Rect(0, 0, rtbWidth, rtbHeight));
+
+            // Draw the image into the computed rect
+            dc.DrawImage(source, destRect);
+
+            rtb.Render(dv);
+
+            stride = rtbWidth * (rtb.Format.BitsPerPixel / 8);
+            pixelWidth = rtb.PixelWidth;
+            pixelHeight = rtb.PixelHeight;
+
+            byte[] pixels = new byte[stride * rtbHeight];
+            rtb.CopyPixels(pixels, stride, 0);
+            return pixels;
+        }
+
+        private static Rect CalculateImageStretchRect(Stretch stretch, double sourceWidth, double sourceHeight, double targetWidth, double targetHeight)
+        {
+            switch (stretch)
+            {
+                case Stretch.None:
+                    return new Rect(0, 0, Math.Min(sourceWidth, targetWidth), Math.Min(sourceHeight, targetHeight));
+
+                case Stretch.Fill:
+                    return new Rect(0, 0, targetWidth, targetHeight);
+
+                case Stretch.Uniform:
+                {
+                    double ratio = Math.Min(targetWidth / sourceWidth, targetHeight / sourceHeight);
+                    double width = sourceWidth * ratio;
+                    double height = sourceHeight * ratio;
+                    double x = (targetWidth - width) / 2;
+                    double y = (targetHeight - height) / 2;
+                    return new Rect(x, y, width, height);
+                }
+
+                case Stretch.UniformToFill:
+                {
+                    double ratio = Math.Max(targetWidth / sourceWidth, targetHeight / sourceHeight);
+                    double width = sourceWidth * ratio;
+                    double height = sourceHeight * ratio;
+                    double x = (targetWidth - width) / 2;
+                    double y = (targetHeight - height) / 2;
+                    return new Rect(x, y, width, height);
+                }
+
+                default:
+                    return new Rect(0, 0, targetWidth, targetHeight);
+            }
+        }
+    }
+}
