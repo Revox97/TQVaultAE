48b303dfea769ad40ec5100efb1413c14b605299
unknown <Leopold.Pohlmann@gmx.net>
2025-10-10 17:26:49 +0200

    add: Avalonia project

---
+++ src/TQVaultAE/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TQVaultAE.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
